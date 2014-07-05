5d6d6dea7186d31d941f2db4c33a1c74b276c893
sven <stankonlines@gmail.com>
2014-07-05 14:06:39 +0900

    added omitted proof.

--- Poly.v
+++ Poly.v
@@ -939,6 +939,7 @@ end.
 Example test_flat_map1:
   flat_map (fun n => [n;n;n]) [1;5;4]
   = [1; 1; 1; 5; 5; 5; 4; 4; 4].
+Proof. reflexivity. Qed.
 
 
 (** Lists are not the only inductive type that we can write a
